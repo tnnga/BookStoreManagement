c4726075cc379063ad09f848c95a372a158c2b39
Trần Thành Đạt <tthanhdat05102003@gmail.com>
2023-05-29 18:14:52 +0700

    1

--- kthp/chiTietTaiKhoan.Designer.cs
+++ kthp/chiTietTaiKhoan.Designer.cs
@@ -342,6 +342,7 @@
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "chiTietTaiKhoan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Phần mềm quản lí nhà sách";
             this.Load += new System.EventHandler(this.chiTietTaiKhoan_Load);
             this.panel1.ResumeLayout(false);
